a20f7a25f3a3158e88f06a5fc5e525866925b063
ygorp <ygor-nogueira@hotmail.com>
2023-08-23 16:51:20 -0300

    estilizado a div resultado, criei link gerar, e o logout

--- orcamento/urls.py
+++ orcamento/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from app_orcamento import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('cadastro/', views.cadastro_usuario, name='cadastro'),
     path('', views.fazer_login, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
